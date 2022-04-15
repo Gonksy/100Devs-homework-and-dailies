7965b2560ee7ce598a49339c5e591f1f6a691d80
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-15 12:14:01 +0200

    Codewars dailies 15/4/2022

--- codewars/03_march/cw_15_4_2022/app.js
+++ codewars/03_march/cw_15_4_2022/app.js
@@ -1,1 +1,28 @@
-
+//https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+//Alan Partridge II - Apple Turnover
+function apple(x){
+  if (x*x > 1000) return "It's hotter than the sun!!"
+  return "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+//https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
+//Remove duplicates from list
+function distinct(a) {
+  let filtered = []
+  a.forEach(x => {
+    if (!filtered.includes(x)) filtered.push(x)
+  })
+  return filtered;
+}
+//https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+//Convert to Binary
+function toBinary(n){
+  let remainderArray = [];
+  let numQuotient = n;
+  let numRemainder = 0;
+  while(numQuotient > 0){
+    numRemainder = numQuotient % 2
+    remainderArray.push(numRemainder)
+    numQuotient = ~~(numQuotient/2);
+  }
+  return Number(remainderArray.reverse().join(''))
+}
