77134ba025189c9e69681773f7be42511efca238
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-16 12:39:27 +0200

    Codewars dailies 16/4/2022

--- codewars/03_march/cw_16_4_2022/app.js
+++ codewars/03_march/cw_16_4_2022/app.js
@@ -1,1 +1,9 @@
+//https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+//Find Multiples of a Number
+function findMultiples(integer, limit) {
+  let array1 = [integer]
+  while ((array1[array1.length-1] + integer) <= limit) array1.push(integer+integer)
+  return array1
+}
 
+console.log(findMultiples(5, 15))
