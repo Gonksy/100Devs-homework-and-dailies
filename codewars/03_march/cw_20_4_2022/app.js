e2d1d09843dcb8dd8c8c6315dbd08dae5deab8f2
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-20 06:23:48 +0200

    Codewars dailies 20/4/2022

--- codewars/03_march/cw_20_4_2022/app.js
+++ codewars/03_march/cw_20_4_2022/app.js
@@ -1,1 +1,16 @@
+//https://www.codewars.com/kata/545afd0761aa4c3055001386/train/javascript
+//Enumerable Magic #25 - Take the First N Elements
+function take(arr, n) {
+  return arr.slice(0, n)
+}
 
+//https://www.codewars.com/kata/570a6a46455d08ff8d001002/train/javascript
+//No zeros for heros
+function noBoringZeros(n) {
+  if (!n) return 0
+  let x = n
+  while (x % 10 == 0){
+    x = x/10
+  }
+  return x
+}
