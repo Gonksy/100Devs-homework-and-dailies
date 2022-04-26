38717e6ee88600fb75e4f166b666c7f6833a0757
Ahmad Yosry <realbulletdragon@gmail.com>
2022-04-26 20:12:16 +0200

    Codewars dailies 25/4/2022 compensation

--- codewars/04_april/cw_25_4_2022/app.js
+++ codewars/04_april/cw_25_4_2022/app.js
@@ -1,1 +1,20 @@
+//https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+//Do you speak "English"?
+function spEng(sentence){
+  let sentenceLower = sentence.toLowerCase();
+  let result = false
+  if ( sentenceLower.includes('english') ) result = true
+  return result
+}
 
+//https://www.codewars.com/kata/57e1e61ba396b3727c000251/train/javascript
+//String cleaning
+function stringClean(s){
+  return s.replace(/[0-9]/g, '')
+}
+
+//https://www.codewars.com/kata/5641a03210e973055a00000d/train/javascript
+//Formatting decimal places #0
+function twoDecimalPlaces(n) {
+  return +(Math.round(n * 100) / 100).toFixed(2)
+}
