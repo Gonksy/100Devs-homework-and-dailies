ed5de318af3e531d46bb7485c1564f3ee893b151
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-01 20:46:54 +0200

    Codewars dailies 1/5/2022

--- Coding Challenges/codewars/05_may/cw_01_5_2022/app.js
+++ Coding Challenges/codewars/05_may/cw_01_5_2022/app.js
@@ -1,1 +1,27 @@
+//https://www.codewars.com/kata/54fe05c4762e2e3047000add/train/javascript
+//Object Oriented Piracy
+function Ship(draft,crew) {
+  this.draft = draft;
+  this.crew = crew;
 
+  this.isWorthIt = function(){
+    let peopleWeight = crew*1.5
+    let difference = draft - peopleWeight
+    if (difference > 20) return true
+    return false
+  }
+}
+
+//https://www.codewars.com/kata/57cff961eca260b71900008f/train/javascript
+//Is there a vowel in there?
+const vowels = {
+  097: 'a',
+  101: 'e',
+  105: 'i',
+  111: 'o',
+  117: 'u'
+}
+
+function isVow(a){
+ return a.map(x => vowels[x] || x)
+}
