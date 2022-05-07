83bdd8c7f41b8833c8ee4d94825cef6b05a0791d
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-07 04:13:32 +0200

    Codewars dailies 06 and 07/05/2022

--- Coding Challenges/codewars/05_may/cw_06_5_2022/app.js
+++ Coding Challenges/codewars/05_may/cw_06_5_2022/app.js
@@ -1,1 +1,16 @@
-
+//https://www.codewars.com/kata/5552101f47fc5178b1000050/train/javascript
+//Playing with digits
+function digPow(n, p){
+  let digits = n.toString().split('').map(x => +x)
+  let sum = digits.map((x, i) => x**(p+i)).reduce((acc,c) => acc+c, 0)
+  let result = -1
+  
+  for (let i = 1; i < 100000; i++){
+    if (n * i == sum) {
+      result = i
+      break
+    }
+  }
+  console.log({digits, sum, result})
+  return result
+}
--- Coding Challenges/codewars/05_may/cw_07_5_2022/app.js
+++ Coding Challenges/codewars/05_may/cw_07_5_2022/app.js
@@ -1,1 +1,6 @@
-
+//https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+//Descending Order
+function descendingOrder(n){
+  let nReverse = +n.toString().split('').sort((a,b)=> b-a).join('')
+  return nReverse
+}
