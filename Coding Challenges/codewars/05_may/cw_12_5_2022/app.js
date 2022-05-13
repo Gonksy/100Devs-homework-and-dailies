cf54733b2fc06904ccbe6b0c78384c79ec51b619
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-13 09:28:19 +0200

    Codewars dailies 12-5-2022

--- Coding Challenges/codewars/05_may/cw_12_5_2022/app.js
+++ Coding Challenges/codewars/05_may/cw_12_5_2022/app.js
@@ -1,1 +1,13 @@
+//https://www.codewars.com/kata/57ee99a16c8df7b02d00045f/train/javascript
+//Flatten and sort an array
+function flattenAndSort(array) {
+  let result = []
+  
+  array.forEach(x => {
+    if (x.length !== 0) result.push(...x)
+  })
+  result.sort((a,b) => a-b)
+  
+  return result;
+}
 
