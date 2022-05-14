892a5823685ef7c52006ede87182d43b1d7f62e0
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-14 09:15:00 +0200

    Codewars dailies 14-05/2022

--- Coding Challenges/codewars/05_may/cw_14_5_2022/app.js
+++ Coding Challenges/codewars/05_may/cw_14_5_2022/app.js
@@ -1,1 +1,24 @@
+//https://www.codewars.com/kata/56d49587df52101de70011e4/train/javascript
+//Leonardo Dicaprio and Oscars
+function leo(oscar){
+  if (oscar > 88) return "Leo got one already!"
+  if (oscar === 88) return "Leo finally won the oscar! Leo is happy"
+  if (oscar === 86) return "Not even for Wolf of wallstreet?!"
+  return "When will you give Leo an Oscar?"
+}
 
+//https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+//Isograms
+function isIsogram(str){
+  let arrLow =  str.toLowerCase().split('')
+  let counts = arrLow.reduce((obj, c) => {
+    if (!obj[c]) obj[c] = 0
+    obj[c]++
+    return obj
+  }, {})
+  
+  let status = Object.keys(counts).findIndex(key => counts[key] > 1)
+  
+  if (status === -1) return true
+  return false
+}
