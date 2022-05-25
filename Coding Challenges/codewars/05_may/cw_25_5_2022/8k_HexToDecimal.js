5b7a7bb25f25ab1588c82abe537a6078ef43a8b7
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-25 11:12:59 +0200

    Codewars dailies - 8k - HexToDecimal

---
+++ Coding Challenges/codewars/05_may/cw_25_5_2022/8k_HexToDecimal.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+// Complete the function which converts hex number (given as a string) to a decimal number.
+function hexToDec(hexString){
+  let decimalNum = parseInt(hexString, 16)
+  return decimalNum
+}
