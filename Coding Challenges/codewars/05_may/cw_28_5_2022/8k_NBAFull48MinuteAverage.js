bc4eb2beaa29587113720393ba204f18dbe6f662
Ahmad Yosry <realbulletdragon@gmail.com>
2022-05-28 11:21:19 +0200

    Codewars dailies - 8k - NBAFull48MinutesAverage

---
+++ Coding Challenges/codewars/05_may/cw_28_5_2022/8k_NBAFull48MinuteAverage.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/587c2d08bb65b5e8040004fd/train/javascript
+// An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game, subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+
+// Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+// Examples:
+
+// pointsPer48(12, 20) // 28.8
+// pointsPer48(10, 10) // 48 
+// pointsPer48(5, 17) // 14.1 
+// pointsPer48(0, 0) // 0
+// Notes:
+// All inputs will be either be an integer or float.
+// Follow your dreams!
+
+function pointsPer48(pointsPerGame, minutesPerGame) {
+  if (pointsPerGame === 0 || minutesPerGame === 0) return 0
+  let pointsPer48Minutes = (pointsPerGame*48) / minutesPerGame
+  return +pointsPer48Minutes.toFixed(1)
+}
