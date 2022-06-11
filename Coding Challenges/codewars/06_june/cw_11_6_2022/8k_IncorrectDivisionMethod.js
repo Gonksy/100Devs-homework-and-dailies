0d9e4542ba2e6f701e36696fede1b5ee5d34a3c7
Ahmad Yosry <realbulletdragon@gmail.com>
2022-06-11 19:30:02 +0200

    Codewars dailies - 8k - IncorrectDivisionMethod

---
+++ Coding Challenges/codewars/06_june/cw_11_6_2022/8k_IncorrectDivisionMethod.js
@@ -0,0 +1,3 @@
+// https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/javascript
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+const solve = (x, y) => x/y
